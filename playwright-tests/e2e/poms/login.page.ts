901dfa009c5b41dc2bd157192e513ff15fc82631
vigbi <bishoyibighneswar@gmail.com>
2024-07-08 20:36:29 +0530

    Added POM login.page.ts

---
+++ playwright-tests/e2e/poms/login.page.ts
@@ -0,0 +1,21 @@
+import { Page, expect } from '@playwright/test';
+import BasePage from '../poms/base.page';
+
+enum Selectors {
+  emailInput = 'login-email-text-field',
+  loginCodeInput = 'otpinput-otp-number',
+  nextBtn = 'login-submit-button'
+}
+
+export default class LoginPage extends BasePage {
+
+  constructor(page: Page) {
+    super(page);
+  }
+
+  async login(email: string, otp: string) {
+    await this.type(Selectors.emailInput, email);
+    await this.click(Selectors.nextBtn);
+    await this.type(Selectors.loginCodeInput, otp);
+  }
+}
